18e0cd13d46d2cf7949b9aa81035cba954fd9be8
vibhor2004may <tyagivibhor86@gmail.com>
2026-01-20 11:35:52 +0530

    Add files via upload

---
+++ horizontal and vertical.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int rows = 5; 
+
+    for (i = 0; i < rows; i++) {
+        printf("l\n"); 
+    }
+for (i=1; i<=4; i++){
+    printf("l ");
+}
+
+    return 0;
+}
