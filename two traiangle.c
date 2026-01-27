174bb61fadaefd28d0f91021b2e170c5c96e42d6
vibhor2004may <tyagivibhor86@gmail.com>
2026-01-27 12:03:24 +0530

    Add files via upload

---
+++ two traiangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 3;
+
+    for (int i = 1; i <= rows; i++) {
+
+        for (int j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("   ");  
+        for (int j = 1; j <= rows - i; j++) {
+            printf("  ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
